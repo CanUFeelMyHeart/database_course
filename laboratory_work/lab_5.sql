7ab136159264088448e6bb11ef021132018eeb89
CanUFeelMyHEart <msmerkushov123@yandex.ru>
2021-05-19 00:57:22 +0300

    create 3 procedure

--- laboratory_work/lab_5.sql
+++ laboratory_work/lab_5.sql
@@ -109,3 +109,63 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY 'n'
 IGNORE 1 ROWS;
+
+-- Процедуры
+
+DELIMITER //
+
+CREATE PROCEDURE FirstProcedure()
+BEGIN 
+	SELECT 'This is my first procedure';
+END //
+DELIMITER ; 
+
+CALL FirstProcedure();
+
+-- 1.
+
+-- Выведем при помощи процедуры информацию о карте в диапазоне
+-- Выбираем данные, которые нужны, при помощи SELECT
+-- Задаем условие выборки: у конкретной карты по id-конкретный тип продукта 
+-- Возьмем не сильно объемную часть выборки - (5;100)
+
+DELIMITER //
+CREATE PROCEDURE CardType() 
+BEGIN 
+	SELECT  product.id, `name`, connection_date
+    FROM product_type
+    JOIN product ON product_type.id = product.id
+    WHERE product.id>5 AND product.id<100;
+END //
+DELIMITER ;
+CALL CardType();
+
+-- 2
+
+-- Выведем максимальную зарплату специалиста
+
+DELIMITER //
+CREATE PROCEDURE maximumPrize(OUT salary INT)
+BEGIN 
+	SELECT MAX(salary)
+    INTO salary
+    FROM specialist;
+END //
+DELIMITER ;
+
+CALL maximumPrize(@MaxSalary);
+SELECT @MaxSalary;
+
+-- 3.
+
+-- Выведем кота, который продается в магазине "Четыре лапы" и id
+
+DELIMITER //
+CREATE PROCEDURE catsAndShop()
+BEGIN
+	SELECT *
+    FROM shops
+    WHERE shopname = "Четыре лапы";
+END //
+CALL catsAndShop();
+    
